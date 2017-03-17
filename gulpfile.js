5cda6cc539923a8cfa28ee7d5d1945d314e039b1
Subash Maharjan <hudeomah@gmail.com>
2017-03-17 17:23:10 -0400

    gulp configured and sass configured

--- gulpfile.js
+++ gulpfile.js
@@ -8,36 +8,36 @@ var imagemin = require('gulp-imagemin');
 var pngquant = require('imagemin-pngquant');
 
 gulp.task('imagemin', function () {
-    return gulp.src('./themes/custom/mars/images/*')
+    return gulp.src('./themes/custom/bodhi/images/*')
         .pipe(imagemin({
             progressive: true,
             svgoPlugins: [{removeViewBox: false}],
             use: [pngquant()]
         }))
-        .pipe(gulp.dest('./themes/custom/mars/images'));
+        .pipe(gulp.dest('./themes/custom/bodhi/images'));
 });
 
 gulp.task('sass', function () {
-  gulp.src('./themes/custom/mars/sass/**/*.scss')
+  gulp.src('./themes/custom/bodhi/scss/**/*.scss')
     .pipe(sourcemaps.init())
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
     .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('./themes/custom/mars/css'));
+    .pipe(gulp.dest('./themes/custom/bodhi/css'));
 });
 
 gulp.task('uglify', function() {
-  gulp.src('./themes/custom/mars/lib/*.js')
+  gulp.src('./themes/custom/bodhi/lib/*.js')
     .pipe(uglify('main.js'))
-    .pipe(gulp.dest('./themes/custom/mars/js'))
+    .pipe(gulp.dest('./themes/custom/bodhi/js'))
 });
 
 gulp.task('watch', function(){
     livereload.listen();
 
-    gulp.watch('./themes/custom/mars/sass/**/*.scss', ['sass']);
-    gulp.watch('./themes/custom/mars/lib/*.js', ['uglify']);
-    gulp.watch(['./themes/custom/mars/css/*.css', './themes/custom/mars/**/*.twig', './themes/custom/mars/js/*.js'], function (files){
+    gulp.watch('./themes/custom/bodhi/scss/**/*.scss', ['sass']);
+    gulp.watch('./themes/custom/bodhi/lib/*.js', ['uglify']);
+    gulp.watch(['./themes/custom/bodhi/css/*.css', './themes/custom/bodhi/**/*.twig', './themes/custom/bodhi/js/*.js'], function (files){
         livereload.changed(files)
     });
 });
